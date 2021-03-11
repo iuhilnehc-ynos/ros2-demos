6f52e4e9c7349f61b61befaeceafcd45f6370774
Chen Lihui <lihui.chen@sony.com>
2021-03-11 14:46:52 +0800

    update to support complicated cft case
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>

--- demo_nodes_cpp/src/topics/listener.cpp
+++ demo_nodes_cpp/src/topics/listener.cpp
@@ -37,39 +37,54 @@ public:
       [this](const std_msgs::msg::String::SharedPtr msg) -> void
       {
         RCLCPP_INFO(this->get_logger(), "I heard: [%s]", msg->data.c_str());
+        static bool once = true;
+        static int i = 0;
+        if (once) {
+          std::string key = "Hello World: ";
+          std::size_t pos = msg->data.find(key);
+          i = std::stoi(msg->data.substr (pos + key.length()));
+          // make the number as multiples of 5 or 10
+          i = ((i / 5) + 1) * 5;
+          once = false;
+        }
 
-        // To get filter expression
-        // std::string filter_expression;
-        // std::vector<std::string> expression_parameters;
-        // sub_->get_cft_expression_parameters(filter_expression, expression_parameters);
-        // RCLCPP_INFO(this->get_logger(), "filter_expression: [%s]", filter_expression.c_str());
-        // for(auto &expression_parameter: expression_parameters) {
-        //   RCLCPP_INFO(this->get_logger(), "expression_parameter: [%s]", expression_parameter.c_str());
-        // }
+        if (i < 100) {
+          // To update with a new filter expression
+          std::string filter_expression = "data MATCH %0";
+          std::string expression_parameter = "'Hello World: " + std::to_string(i) + "'";
 
-        // To update with a new filter expression
-        // std::string filter_expression_new = "data MATCH %0";
-        // std::string expression_parameter = "'Hello World: 8'";
-        // RCLCPP_INFO(this->get_logger(), "update cft expression parameter : [%s][%s]",
-        //   filter_expression_new.c_str(), expression_parameter.c_str());
-        // sub_->set_cft_expression_parameters(filter_expression_new, {expression_parameter});
+          RCLCPP_INFO(this->get_logger(), "set_cft_expression_parameters : [%s][%s]",
+            filter_expression.c_str(), expression_parameter.c_str());
+          sub_->set_cft_expression_parameters(filter_expression, {expression_parameter});
+          i += 5;
 
-        // (HOW TO RESET SUBSCRIPTION) expression_parameters can't be reset by an empty string.
-        // [listener-2] [D0028|SET CF PARAMS]DDS_SqlFilterparse:syntax error, unexpected $end
-        // [listener-2] [D0028|SET CF PARAMS]DDS_SqlFilter_compile:SQL compiler failed with error-code: -1 (Syntax error)
-        // [listener-2] [D0028|SET CF PARAMS]PRESContentFilteredTopic_updateFilterExpression:content filter compile error 1
-        // [listener-2] [ERROR] [1613978782.142065462] [rmw_connextdds]: failed to set content-filtered topic
-        // sub_->set_cft_expression_parameters("", {});
+          // To get filter expression
+          try {
+            std::string filter_expression_tmp;
+            std::vector<std::string> expression_parameters;
+            sub_->get_cft_expression_parameters(filter_expression_tmp, expression_parameters);
+            RCLCPP_INFO(this->get_logger(), "get_cft_expression_parameters filter_expression: [%s]", filter_expression_tmp.c_str());
+            for(auto &expression_parameter: expression_parameters) {
+              RCLCPP_INFO(this->get_logger(), "get_cft_expression_parameters expression_parameter: [%s]", expression_parameter.c_str());
+            }
+          } catch (const std::exception& e) {
+            RCLCPP_INFO(this->get_logger(), "get_cft_expression_parameters exception: %s", e.what());
+          }
+        } else {
+          // reset
+          RCLCPP_INFO(this->get_logger(), "reset cft");
+          sub_->set_cft_expression_parameters("", {});
+        }
       };
     // Create a subscription to the topic which can be matched with one or more compatible ROS
     // publishers.
     // Note that not all publishers on the same topic with the same type will be compatible:
     // they must have compatible Quality of Service policies.
-    rclcpp::SubscriptionOptionsBase options_base;
-    options_base.content_filter_options.filter_expression
-      = "data MATCH 'Hello World: 5' or data MATCH %0";
-    options_base.content_filter_options.expression_parameters = {"'Hello World: 10'"};
-    rclcpp::SubscriptionOptionsWithAllocator<std::allocator<void>> subscription_options(options_base);
+
+    rclcpp::SubscriptionOptions subscription_options;
+    // subscription_options.content_filter_options.filter_expression
+    //   = "data MATCH 'Hello World: 5' or data MATCH %0";
+    // subscription_options.content_filter_options.expression_parameters = {"'Hello World: 10'"};
     sub_ = create_subscription<std_msgs::msg::String>("chatter", 10, callback, subscription_options);
   }
 
